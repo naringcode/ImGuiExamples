e9b86f590392506b85622632b80cd003d1c1e0d8
naringcode <naringcode@gmail.com>
2023-11-06 19:41:34 +0900

    demo

--- 01_Demo/src/main.cpp
+++ 01_Demo/src/main.cpp
@@ -1,8 +1,212 @@
+// C++
 #include <iostream>
 
-int main()
+// imgui
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdlrenderer2.h>
+
+// SDL
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[])
 {
-    std::cout << "First Commit Test" << '\n';
+    // Setup SDL
+    if (0 != SDL_Init(SDL_INIT_EVERYTHING))
+    {
+        std::cout << "[Error] SDL_Init() : " << SDL_GetError();
+
+        return -1;
+    }
+
+    // From 2.0.18: Enable native IME.
+    // https://wiki.libsdl.org/SDL2/CategoryHints <- where is it?
+    // https://discourse.libsdl.org/t/sdl-2-0-18-released/33768
+#ifdef SDL_HINT_IME_SHOW_UI
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#endif
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    window = SDL_CreateWindow("Hello ImGui + SDL2 Demo",
+                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              1280, 720,
+                              SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+
+    if (nullptr == window)
+    {
+        SDL_Quit();
+
+        std::cout << "[Error] SDL_CreateWindow() : " << SDL_GetError();
+
+        return -1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    
+    if (nullptr == renderer)
+    {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        std::cout << "[Error] SDL_CreateWindow() : " << SDL_GetError();
+
+        return -1;
+    }
+
+    // SDL_RendererInfo info;
+    // SDL_GetRendererInfo(renderer, &info);
+    // std::cout << "[LOG] Current SDL_Renderer : " << info.name;
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    {
+        (void)io;
+
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;   // Enable Keyboard Controls
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;    // Enable Gamepad Controls
+    }
+
+    // Setup Dear ImGui style
+    // ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    ImGuiStyle style;
+    {
+        style.Alpha = 0.2f;
+    }
+
+    ImGui::StyleColorsDark(&style);
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != nullptr);
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+
+    // States
+    bool showDemoWindow = true;
+    bool showAnotherWindow = true;
+
+    ImVec4 clearColor(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main Logic
+    bool isRunning = true;
+    while (true == isRunning)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+
+            if (SDL_QUIT == event.type)
+            {
+                isRunning = false;
+
+                break;
+            }
+            else if (SDL_KEYDOWN == event.type)
+            {
+                if (SDLK_ESCAPE == event.key.keysym.sym)
+                {
+                    isRunning = false;
+
+                    break;
+                }
+            }
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()!
+        //   You can browse its code to learn more about Dear ImGui!).
+        if (showDemoWindow)
+        {
+            ImGui::ShowDemoWindow(&showDemoWindow);
+        }
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");           // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &showDemoWindow);    // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &showAnotherWindow);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clearColor);  // Edit 3 floats representing a color
+
+            // Buttons return true when clicked (most widgets return true when edited/activated)
+            if (ImGui::Button("Button"))
+            {
+                counter++;
+            }
+
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (showAnotherWindow)
+        {
+            ImGui::Begin("Another Window", &showAnotherWindow);
+            ImGui::Text("Hello from another window!");
+
+            if (ImGui::Button("Close Me"))
+            {
+                showAnotherWindow = false;
+            }
+
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+        SDL_RenderPresent(renderer);
+    }
+
+    // Cleanup
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
